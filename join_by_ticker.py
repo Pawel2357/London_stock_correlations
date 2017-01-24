a080a519bfa84d515a58272e025b69adfd24557b
pawel.urbanski <pawel.urbanski@tooploox.com>
2017-01-24 21:38:14 +0100

    Define preproces_data and compute_join_by_ticker methods.

--- join_by_ticker.py
+++ join_by_ticker.py
@@ -1,5 +1,23 @@
-def preproces_data(file, [names]):
-    return dataframe [names]
+import argparse
+import pickle
 
-def compute_join_by_ticker(dataframe_1, dataframe_2, col_name):
+
+def preproces_data(filename, col_names):
+    data = pd.read_csv(filename, sep=" ", header = None)
+    data.columns = col_names
+    return data
+
+def compute_join_by_ticker(df_1, df_2, col_name):
+    dataframe = pd.concat([df_1, df_2], axis=1, join_axes=[df_1[col_name]])
     return dataframe
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename_1",
+        help = "txt filename")
+    parser.add_argument("column_names_1",
+        help = "txt filename")
+    parser.add_argument("filename_2",
+        help = "list of column names in pickle")
+    parser.add_argument("column_names_2",
+        help = "list of column names in pickle")
